10599f0837400bb8ceb2740258e4cc1a16f44050
Shane Charles <shanecharles@burningicesolutions.com>
2014-12-23 08:41:19 -0600

    No longer use DbMessage

--- dbagent.fsx
+++ dbagent.fsx
@@ -4,9 +4,9 @@ type Person = { Id: int; FirstName: string; LastName: string;}
 // Discriminated Union. Think of an Enum with an attached Tuple
 type DbOperation = 
     | Add of Person
-    | Filter of (Person -> bool)
-    | Delete of int
-    | Update of Person
+    | Filter of (Person -> bool) * AsyncReplyChannel<list<Person> >
+    | Delete of int * AsyncReplyChannel<bool>
+    | Update of Person * AsyncReplyChannel<bool>
 
 type DbResult =
     | Results of list<Person>
@@ -18,37 +18,45 @@ type DbMessage =
 let personServerAgent = MailboxProcessor.Start(fun inbox ->
     let rec loop oldState =
         async {
-             let! (DbMessage(m,c)) = inbox.Receive()
-             let newState, result, msg = 
+             let! (m) = inbox.Receive()
+             let newState = 
                  match m with 
-                 | Add(p) -> (p::oldState, true, None)
-                 | Filter(func) -> (oldState, true, Some((List.filter func oldState)))
-                 | Delete(id) ->
-                    (List.filter (fun p -> p.Id <> id) oldState), true, None
-                 | Update(p) ->
-                    (p::(List.filter (fun p1 -> p1.Id <> p.Id) oldState)), true, None
+                 | Add(p) -> p::oldState
+                 | Filter(func, c) ->
+                        
+                     c.Reply(List.filter func oldState)
+                     oldState
+                 | Delete(id, c) ->
+                    (List.filter (fun p -> p.Id <> id) oldState)
+                 | Update(p, c) ->
+                     match oldState |> List.exists (fun e -> e.Id = p.Id) with
+                     | true ->
+                         c.Reply(false)
+                         oldState
+                     | _    ->
+                         c.Reply(true)
+                         p :: (oldState |> List.filter (fun p1 -> p1.Id <> p.Id))
 
-             c.Reply (result, msg)
              return! loop newState
          }
     loop [])
 
 
 let populate = 
-    personServerAgent.PostAndReply(fun c -> DbMessage(Add({Id=1;FirstName="Sarah";LastName="Smith"}),c)) |> ignore
-    personServerAgent.PostAndReply(fun c -> DbMessage(Add({Id=2;FirstName="Kevin";LastName="Daily"}),c)) |> ignore
-    personServerAgent.PostAndReply(fun c -> DbMessage(Add({Id=3;FirstName="Mance";LastName="Watson"}),c)) |> ignore
-    personServerAgent.PostAndReply(fun c -> DbMessage(Add({Id=4;FirstName="Brett";LastName="Islander"}),c)) |> ignore
-    personServerAgent.PostAndReply(fun c -> DbMessage(Add({Id=5;FirstName="Mary";LastName="Watson"}),c)) |> ignore
+    personServerAgent.Post(Add({Id=1;FirstName="Sarah";LastName="Smith"})) |> ignore
+    personServerAgent.Post(Add({Id=2;FirstName="Kevin";LastName="Daily"})) |> ignore
+    personServerAgent.Post(Add({Id=3;FirstName="Mance";LastName="Watson"})) |> ignore
+    personServerAgent.Post(Add({Id=4;FirstName="Brett";LastName="Islander"})) |> ignore
+    personServerAgent.Post(Add({Id=5;FirstName="Mary";LastName="Watson"})) |> ignore
 
 let updatePerson person =
-    personServerAgent.PostAndReply(fun c -> DbMessage(Update(person), c))
+    personServerAgent.PostAndReply(fun c -> Update(person,c))
 
 let deleteById id =
-    personServerAgent.PostAndReply(fun c -> DbMessage(Delete(id),c))
+    personServerAgent.PostAndReply(fun c -> Delete(id,c))
 
 let getByLastName name = 
-    personServerAgent.PostAndReply(fun c -> DbMessage(Filter(fun r -> r.LastName.Equals(name)),c))
+    personServerAgent.PostAndReply(fun c -> Filter((fun r -> r.LastName.Equals(name)),c))
 
 let getById id = 
-    personServerAgent.PostAndReply(fun c -> DbMessage(Filter(fun r -> r.Id = id),c))
+    personServerAgent.PostAndReply(fun c -> Filter((fun r -> r.Id = id),c))
